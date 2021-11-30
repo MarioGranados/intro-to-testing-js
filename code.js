2f633696853f0f0935c33073dafe15ff5c8c6f7a
Mario Granados <mario.alberto.granados1@gmail.com>
2021-11-30 15:24:45 -0600

    just learned testing

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(name) {
+    return "hello" + name;
+}
+
+function double(num) {
+    return num * 2;
+}
--- tests.js
+++ tests.js
@@ -1,15 +1,43 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+//sayHello test
+describe('sayHello', function () {
+    it('should be defined', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should not return the undefined ', function () {
+        expect(sayHello()).not.toBe('undefined');
+    });
+});
+
+
+//unit test for double
+
+describe('double', function () {
+    it('should return a number when used', function () {
+        expect(typeof double()).toBe('number');
+    });
+    it('should return a function', function () {
+        expect(typeof double).toBe('function');
+    });
+    it('should return 10', function () {
+        expect(double(5)).toEqual(10);
     });
 });
